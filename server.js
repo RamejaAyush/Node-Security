cbdb6ede1453945e24a8a575bb2e80c44082f94f
Ayush Rameja <ayushrwork@gmail.com>
2023-04-11 10:50:29 +0530

    Added files

---
+++ server.js
@@ -0,0 +1,28 @@
+const fs = require("fs")
+const path = require("path")
+const https = require("https")
+const express = require("express")
+
+const app = express()
+
+const PORT = 3000
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+app.get("/secret", (req, res) => {
+  return res.send("Your Secret Value is: GGEZNOOB")
+})
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`server is rinning on https://localhost:${PORT}`)
+  })
