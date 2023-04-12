98b19a43717104caa96e2af4bf873a2f5e012284
Ayush Rameja <ayushrwork@gmail.com>
2023-04-12 17:40:30 +0530

    Final code

--- server.js
+++ server.js
@@ -1,87 +1,121 @@
-require("dotenv").config()
-const fs = require("fs")
-const path = require("path")
-const https = require("https")
-const helmet = require("helmet")
-const express = require("express")
-const passport = require("passport")
-const { Strategy } = require("passport-google-oauth20")
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const helmet = require('helmet');
+const express = require('express');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const { Strategy } = require('passport-google-oauth20');
 
-const PORT = 3000
+const PORT = 3000;
 
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
-}
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+};
 
 const AUTH_OPTIONS = {
-  callbackURL: "/auth/google/callback",
+  callbackURL: '/auth/google/callback',
   clientID: config.CLIENT_ID,
   clientSecret: config.CLIENT_SECRET,
-}
+};
 
 const verifyCallback = (accessToken, refreshToken, profile, done) => {
-  console.log(`Google Profile: ${profile}`)
-  done(null, profile)
-}
+  console.log(`Google Profile:`, profile);
+  done(null, profile);
+};
 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
-const app = express()
+/*
+ * We can limit what we store in the brower using the bellow two functions
+ */
 
-app.use(helmet())
-app.use(passport.initialize())
+// Serialize: Save the session to the user to the cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserial: Loading it from the cookie
+passport.deserializeUser((id, done) => {
+  done(null, id);
+});
+
+const app = express();
+
+app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
+
+app.use(passport.initialize());
+// Authenicates the session that being sent to the server, using keys
+app.use(passport.session());
 
 const checkLoggedIn = (req, res, next) => {
-  const isLoggedIn = true // Todo
+  // req.user, passport session that id for us.
+  console.log('current user', req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
   if (!isLoggedIn)
     return res.status(401).json({
-      error: "You must log in!",
-    })
-  next()
-}
+      error: 'You must log in!',
+    });
+  next();
+};
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"))
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["email"],
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
   })
-)
+);
 
 app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/failure",
-    successRedirect: "/",
-    session: false,
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: true,
   }),
   (req, res) => {
-    console.log("Google Called us back!")
+    console.log('Google Called us back!');
   }
-)
+);
 
-app.get("/auth/logout", (req, res) => {})
+app.get('/auth/logout', (req, res) => {
+  // remove req.user from this req and terminates any logged in user, passport inbuild feature
+  req.logout();
+  return res.redirect('/');
+});
 
-app.get("/secret", checkLoggedIn, (req, res) => {
-  return res.send("Your Secret Value is: GGEZNOOB")
-})
+app.get('/secret', checkLoggedIn, (req, res) => {
+  return res.send('Your Secret Value is: GGEZNOOB');
+});
 
-app.get("failure", (req, res) => {
-  return res.send("Failed To log in!")
-})
+app.get('failure', (req, res) => {
+  return res.send('Failed To log in!');
+});
 
 https
   .createServer(
     {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
     },
     app
   )
   .listen(PORT, () => {
-    console.log(`server is running on https://localhost:${PORT}`)
-  })
+    console.log(`server is running on https://localhost:${PORT}`);
+  });
