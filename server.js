ae7eaf2dc39155dc729a5da433fa201e16e2fd19
Ayush Rameja <ayushrwork@gmail.com>
2023-04-12 14:29:59 +0530

    Implemented OAuth 2.0 using Google with basic callback and passport

--- server.js
+++ server.js
@@ -1,18 +1,77 @@
+require("dotenv").config()
 const fs = require("fs")
 const path = require("path")
 const https = require("https")
+const helmet = require("helmet")
 const express = require("express")
+const passport = require("passport")
+const { Strategy } = require("passport-google-oauth20")
+
+const PORT = 3000
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+}
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log(`Google Profile: ${profile}`)
+  done(null, profile)
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
 const app = express()
 
-const PORT = 3000
+app.use(helmet())
+app.use(passport.initialize())
+
+const checkLoggedIn = (req, res, next) => {
+  const isLoggedIn = true // Todo
+  if (!isLoggedIn)
+    return res.status(401).json({
+      error: "You must log in!",
+    })
+  next()
+}
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
-app.get("/secret", (req, res) => {
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+)
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google Called us back!")
+  }
+)
+
+app.get("/auth/logout", (req, res) => {})
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your Secret Value is: GGEZNOOB")
+})
+
+app.get("failure", (req, res) => {
+  return res.send("Failed To log in!")
 })
 
 https
